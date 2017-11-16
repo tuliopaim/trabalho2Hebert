4429c155c789775a3ea87e8e4cd4ae0dfdb107de
Unknown <tutpaim@gmail.com>
2017-11-16 16:39:27 -0200

    update com funções
    faltando a principal

--- codigos/T3D.c
+++ codigos/T3D.c
@@ -5,17 +5,18 @@ As funções implementadas por Matheus fecham o arquivo depois que elas acabam
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "T3D.h"
 
 /*
 TIPOS DE DADOS
 */
 struct mat4x1{
-	double x,y,z;
+	double vetor[4];
 };
 
 struct mat4x4{
-	double matrix[4][4];
+	double matriz[4][4];
 };
 
 
@@ -32,31 +33,118 @@ void Cria(Mat4x1 *Obj, char* fName){
 		return ;
 	}
 	//lendo e preenchendo a matriz 4x1
-	fscanf(arq,"%lf %lf %lf",&Obj->x,&Obj->y,&Obj->z);
+	fscanf(arq,"%lf %lf %lf",&Obj->vetor[0],&Obj->vetor[1],&Obj->vetor[2]);
 	fclose(arq);
 }
 
 Mat4x4 Trans(Mat4x4 M, double deltaX, double deltaY, double deltaZ){
+	int i, j;
+	
+	//CRIA MATRIZ TRANS A SER MULTIPLICADA PELA COORDENADA
+	for(i=0;i<4;i++){
+		for(j=0;j<4;j++){
+			if(i==j) M.matriz[i][j] = 1;
+			else if(i==0 && j==3) M.matriz[i][j] = deltaX;
+			else if(i==1 && j==3) M.matriz[i][j] = deltaY;
+			else if(i==2 && j==3) M.matriz[i][j] = deltaZ;
+			else M.matriz[i][j] = 0;
+		}
+	}
+
+	//mat4x4 * result = multiplicaMatriz(T);
+
+	return M;
 
 }
 
 Mat4x4 Escala(Mat4x4 M, double FX, double FY, double FZ){
+	int i, j;
+	//CRIA MATRIZ ESCALA A SER MULTIPLICADA PELA COORDENADA
+	for(i=0;i<4;i++){
+		for(j=0;j<4;j++){
+			if(i==0 && j==0) M.matriz[i][j] = FX;
+			else if(i==1 && j==1) M.matriz[i][j] = FY;
+			else if(i==2 && j==2) M.matriz[i][j] = FZ;
+			else if(i==3 && j==3) M.matriz[i][j] = 1;
+			else M.matriz[i][j] = 0;
+		}
+	}
+
+	//mat4x4 * result = multiplicaMatriz(T);
+
+	return M;
 
 }
 
 Mat4x4 Rot(Mat4x4 M, int eixo, double angulo){
-
+	int i, j;
+	if(eixo == 'X' || eixo == 'x'){
+		for(i=0;i<4;i++){
+			for(j=0;j<4;j++){
+				if((i==0 && j==0) || (i==3 && j==3)) M.matriz[i][j] = 1;
+				else if((i==1 && j==1) || (i==2 && j==2)) M.matriz[i][j] = cos(angulo);
+				else if(i==1 && j==2) M.matriz[i][j] = -1 * sin(angulo);
+				else if(i==2 && j==1) M.matriz[i][j] = sin(angulo);
+				else M.matriz[i][j] = 0;
+			}
+		}
+	}
+	else if(eixo == 'Y' || eixo == 'y'){
+		for(i=0;i<4;i++){
+			for(j=0;j<4;j++){
+				if((i==1 && j==1) || (i==3 && j==3)) M.matriz[i][j] = 1;
+				else if((i==0 && j==0) || (i==2 && j==2)) M.matriz[i][j] = cos(angulo);
+				else if(i==0 && j==2) M.matriz[i][j] = -1 * sin(angulo);
+				else if(i==2 && j==0) M.matriz[i][j] = sin(angulo);
+				else M.matriz[i][j] = 0;
+			}
+		}
+	}
+	else if(eixo == 'Z' || eixo == 'z'){
+		for(i=0;i<4;i++){
+			for(j=0;j<4;j++){
+				if((i==2 && j==2) || (i==3 && j==3)) M.matriz[i][j] = 2;
+				else if((i==0 && j==0) || (i==1 && j==1)) M.matriz[i][j] = cos(angulo);
+				else if(i==0 && j==1) M.matriz[i][j] = -1 * sin(angulo);
+				else if(i==1 && j==0) M.matriz[i][j] = sin(angulo);
+				else M.matriz[i][j] = 0;
+			}
+		}
+	}
+	return M;
 }
 
 Mat4x4 MatComp(Mat4x4 M1, Mat4x4 M2){
-
+	int x, i, j;
+	Mat4x4 F;
+	for(x=0;x<4;x++){
+		for(i=0;i<4;i++){
+			F.matriz[x][i] = 0;
+			for(j=0;j<4;j++){
+				F.matriz[x][i] += M1.matriz[x][j] * M2.matriz[j][i];
+			}
+		}
+	}
+	return F;
 }
 
 Mat4x1 MatTransf(Mat4x4 M, Mat4x1 P){
-
+	int i, j;
+	double temp;
+	Mat4x1 v;
+	for(i=0;i<4;i++){
+		v.vetor[i] = 0;
+		for(j=0;j<4;j++){
+			v.vetor[i] += P.vetor[j] * M.matriz[j][i];
+			#ifdef DEBUG
+			printf("%.2lf * %.2lf = %.2lf\n", P.vetor[j], M.matriz[j][i], v.vetor[i]);
+			#endif
+		}
+	}
+	return v;
 }
 
-void Imprime(Mat4x1 *Obj, char* fName){
+void ImprimeNoArquivo(Mat4x1 *Obj, char* fName){
 	FILE * arq;
 	//abrindo o arquivo
 	arq = fopen(fName,"a+");
@@ -64,6 +152,47 @@ void Imprime(Mat4x1 *Obj, char* fName){
 		printf("Erro ao abrir o arquivo\n");
 	}
 	//coloca as coordenadas x, y e z no arquivo "Arq"
-	fprintf(arq, "%lf %lf %lf %lf\n",Obj->x,Obj->y,Obj->z);
+	fprintf(arq, "%lf %lf %lf\n",Obj->vetor[0],Obj->vetor[1],Obj->vetor[2]);
 	fclose(arq);
 }
+
+Mat4x1 * AlocaMat4x1(){
+	Mat4x1 * new;
+	//alocando
+	new = malloc(sizeof(Mat4x1));
+	//conferindo se não foi alocado
+	if (new == NULL){
+		printf("Erro ao alocar uma nova matriz 4x1\n");
+		return NULL;
+	}
+	new->vetor[3] = 1;
+	return new;
+}
+
+Mat4x4 * AlocaMat4x4(){
+	Mat4x4 * new;
+	//alocando
+	new = malloc(sizeof(Mat4x4));
+	//conferindo se não foi alocado
+	if (new == NULL){
+		printf("Erro ao alocar uma nova matriz 4x4\n");
+		return NULL;
+	}
+	return new;
+}
+
+void imprimeMat4x1(Mat4x1 * elemento){
+	printf("Coordenadas (x,y,z,t) : (%.2lf,%.2lf,%.2lf,%.2lf)\n",elemento->vetor[0],elemento->vetor[1],elemento->vetor[2], elemento->vetor[3]);
+}
+
+
+void imprimeMat4x4(Mat4x4 * M){
+	int i, j;
+	printf("\tMATRIZ\n");
+	for(i=0;i<4;i++){
+		for(j=0;j<4;j++){
+			printf((j==3) ? "%lf\n" : "%lf ", M->matriz[i][j]);
+		}
+	}
+}
+
