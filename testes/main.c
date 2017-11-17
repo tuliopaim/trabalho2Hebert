c16d4fb27d48c2f2e8482b7db07673261ad69f76
Unknown <tutpaim@gmail.com>
2017-11-17 19:11:09 -0200

    Ordem de operações

--- testes/main.c
+++ testes/main.c
@@ -13,22 +13,20 @@ int main (){
 
 
 	Mat4x1 vet;
-	vet.matriz[0][0] = 15;
+	vet.matriz[0][0] = 0;
 	vet.matriz[1][0] = 0;
-	vet.matriz[2][0] = 15;
+	vet.matriz[2][0] = 0;
 	vet.matriz[3][0] = 1;
 
 	imprimeMat4x1(&vet);
 
+	Mat4x4 fim;
+
+	//TRANSPOSTA 1
 	Mat4x4 MatT;
-	MatT = Trans(MatT, -7.5, -7.5, 0);
+	MatT = Trans(MatT, 7.5, 7.5, 0);
 	printf("Matriz Trans\n");
 	imprimeMat4x4(&MatT);
-
-	//MULTIPLICA AS DUAS
-	Mat4x1 final;
-	final = MatTransf(MatT, vet);
-	imprimeMat4x1(&final);
 
 	//ROTACIONAR
 	Mat4x4 MatRot;
@@ -36,16 +34,19 @@ int main (){
 	printf("Matriz Rot\n");
 	imprimeMat4x4(&MatRot);
 
-	final = MatTransf(MatRot, final);
-	imprimeMat4x1(&final);
+	fim = MatComp(MatT, MatRot); // MULTIPLICA AS DUAS
 
 	//TRANS 2
 	Mat4x4 MatT2;
-	MatT2 = Trans(MatT2, 7.5, 7.5, 0);
+	MatT2 = Trans(MatT2, -7.5, -7.5, 0);
 	printf("Matriz Trans2\n");
 	imprimeMat4x4(&MatT2);
 
-	final = MatTransf(MatT2, final);
+	fim = MatComp(fim, MatT2); // MULTIPLICA AS DUAS
+	
+	//APLICA NO PONTO
+	Mat4x1 final;
+	final = MatTransf(fim, vet);
 	imprimeMat4x1(&final);
 
 
