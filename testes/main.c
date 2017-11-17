2c9fd5f6a4d5e088b0469b9a5885e27bd3759b15
Unknown <tutpaim@gmail.com>
2017-11-17 10:09:19 -0200

    Teste dos Exemplos

--- testes/main.c
+++ testes/main.c
@@ -11,67 +11,37 @@ int main (){
 	arq = fopen("entradas.txt","r");
 	Cria(teste,"entradas.txt");
 
-	imprimeMat4x1(teste);
+	Mat4x4 MatT;
+	MatT = Trans(MatT, 7.5, 7.5, 0);
 
-	//TESTE MatComp
-	Mat4x4 m1;
-	Mat4x4 m2;
+	imprimeMat4x4(&MatT);
 
-	for (int i = 0; i < 4; ++i){
-		for(int j=0;j<4;j++){
-			m1.matriz[i][j] = 1;
-			m2.matriz[i][j] = 1;
-		}
-	}
+	Mat4x4 MatR;
+	MatR = Rot(MatR, 'Z', 30);
 
-	m2 = MatComp(m1, m2);
-	imprimeMat4x4(&m2);
+	imprimeMat4x4(&MatR);
 
-	//TESTE MatTransf
-	Mat4x4 mat;
+	Mat4x4 MatT2;
+	MatT2 = Trans(MatT2, -7.5, -7.5, 0);
+
+	imprimeMat4x4(&MatT2);
+
+	Mat4x4 Fim;
+	Fim = MatComp(MatT, MatR);
+	imprimeMat4x4(&Fim);
+	Fim = MatComp(Fim, MatT2);
+	imprimeMat4x4(&Fim);
+
 	Mat4x1 vet;
+	vet.matriz[0][0] = 0;
+	vet.matriz[1][0] = 15;
+	vet.matriz[2][0] = 15;
+	vet.matriz[3][0] = 1;
 
-	for (int i = 0; i < 4; ++i) vet.matriz[i][0] = 1;
+	Mat4x1 Final;
+	Final = MatTransf(Fim, vet);
 
-	for (int i = 0; i < 4; ++i){
-		for(int j=0;j<4;j++){
-			mat.matriz[i][j] = 1;
-		}
-	}
-
-	vet = MatTransf(mat, vet);
-
-	imprimeMat4x1(&vet);
-
-
-
-	//TESTE ESCALONAMENTO
-	double deltaX,deltaY,deltaZ;
-	printf("Coordenadas de escalonamento:");
-	scanf("%lf %lf %lf", &deltaX, &deltaY, &deltaZ);
-
-	Mat4x4 M = Trans(M, deltaX, deltaY, deltaZ);
-
-	imprimeMat4x4(&M);
-
-	//TESTE TRANSLAÇÃO
-	double FX, FY, FZ;
-	printf("Coordenadas de escala:");
-	scanf("%lf %lf %lf", &FX, &FY, &FZ);
-
-	Mat4x4 S = Escala(S, FX, FY, FZ);
-
-	imprimeMat4x4(&S);
-
-
-	//TESTE ROTAÇÃO
-	char type;
-	double ang, val = PI / 180;
-	scanf(" %c %lf", &type, &ang);
-
-	Mat4x4 R = Rot(R, type, val*ang);
-
-	imprimeMat4x4(&R);
+	imprimeMat4x1(&Final);
 	
 
 	return 0;
