d6ac1b802fa43eb8f85a1279f0ff36b4a11110e3
tuliopaim <tutpaim@gmail.com>
2017-11-21 16:39:27 -0200

    Update main.c

--- codigos/main.c
+++ codigos/main.c
@@ -1,10 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "T3D.h"
 
-int main (){
-	//PASSA O NOME DO ARQUIVO DE ENTRADA E O ARQUIVO DE SAIDA
-	//AS COISAS ACONTECEM
-	MakeItHappen("entradas.txt", "saidas.txt");
-	return 0;
+int main(){
+  int i, j,n;
+
+  //DECLARA O INICIO DE UMA LISTA DE MAT4X1
+  Mat4x1 * cord = lista_inicia();
+  /*FUNÇÃO CRIA LÊ AS COORDENADAS DO ARQUIVO E CRIA UMA LISTA
+  DINAMICA ENCADEADA */
+  Cria(cord, "entradas.txt");
+
+  /*MATRIZ TRANSFORMAÇÃO
+  CRIA UMA STRUCT MAT4X4 PARA ARMAZENAR A MATRIZ TRANSFORMAÇÃO*/
+  Mat4x4 transforma;
+  /*FUNÇÃO pegaMatrizes LÊ AS TRANSFORMAÇÕES NO ARQUIVO, GERA E MULTIPLICA
+  TODAS AS MATRIZES, RETORNA A RESULTANTE*/
+  transforma = pegaMatrizes("entradas.txt");
+
+  //PERCORRE A LISTA DE COORDENADAS E AS MULTIPLICA PELA MATRIZ TRANSFORMAÇÃO
+  perMult(cord, transforma);
+
+  //IMPRIME O NUMERO DE COORDENADAS NO INICIO DO ARQUIVO
+  FILE * entrada = fopen("entradas.txt", "r");
+  FILE * saida = fopen("saidas.txt", "w");
+  fscanf(entrada, "%d", &n);
+  fprintf(saida, "%d\n", n);
+  fclose(entrada);
+  fclose(saida);
+
+  //BURN
+  //IMPRIME AS COORDENADAS TRANSFORMADAS NO ARQUIVO DE SAIDA
+  Imprime(cord,"saidas.txt");
+
+  //Clear the mess, go home
+  //DESALOCA A LISTA CRIADA
+  lista_libera(cord);
+  return 0;
 }
